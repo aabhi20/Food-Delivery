1f446f9a9f0e1ccc13774ac91063dc7a082be208
aabhi/20 <abhaymishramus@gmail.com>
2025-05-21 12:38:52 +0530

    responsive header with color

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,8 +15,8 @@ export default function () {
     <>
       <header className="flex items-center justify-between bg-[#f7f0d6] sm:hidden">
         <div className="flex items-center p-2 sm:hidden bg-[#f7f0d6]  w-[70vw]">
-          <div className="w-24 h-12 border-1 bg-[#f0f0e9] border-[#FFBE0B] border-r-0 rounded-[10px] rounded-r-none">
-            <img src={banner} className="w-24 h-12 " />
+          <div className="flex items-center w-24 h-12 border-1 bg-[#f0f0e9] border-[#FFBE0B] border-r-0 rounded-[10px] rounded-r-none">
+            <img src={banner} className="w-24 h-14 " />
           </div>
           <div className="border-1 border-[#FFBE0B] bg-[#f0f0e9] hover:border-[#CC9500] p-[11px] rounded-[10px] rounded-l-none w-[45vw]">
             <input type="text" placeholder="search..." className="" />
@@ -27,7 +27,7 @@ export default function () {
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
             fill="currentColor"
-            className="size-6"
+            className="size-7"
           >
             <path d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z" />
           </svg>
@@ -36,7 +36,7 @@ export default function () {
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
             fill="currentColor"
-            className="size-6"
+            className="size-7"
           >
             <path d="M2.25 2.25a.75.75 0 0 0 0 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 0 0-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 0 0 0-1.5H5.378A2.25 2.25 0 0 1 7.5 15h11.218a.75.75 0 0 0 .674-.421 60.358 60.358 0 0 0 2.96-7.228.75.75 0 0 0-.525-.965A60.864 60.864 0 0 0 5.68 4.509l-.232-.867A1.875 1.875 0 0 0 3.636 2.25H2.25ZM3.75 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0ZM16.5 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0Z" />
           </svg>
@@ -45,7 +45,7 @@ export default function () {
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
             fill="currentColor"
-            className="size-6"
+            className="size-7"
           >
             <path
               fillRule="evenodd"
