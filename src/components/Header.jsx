f8596ccb5efb17f49abfd98a480d4c2d6ef94847
aabhi/20 <abhaymishramus@gmail.com>
2025-05-22 10:02:07 +0530

    fixed padding in header and navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from "./Navbar";
+import { useState, useRef, useEffect } from "react";
 
 import banner from "../assets/pictures/banner.png";
 import {
@@ -13,7 +14,9 @@ import {
 export default function () {
   return (
     <>
-      <header className="flex items-center justify-between bg-[#f7f0d6] sm:hidden  z-50 fixed w-full">
+      {/* HEADER FOR SMALL SCREEN */}
+
+      <header className="flex items-center justify-between bg-[#f7f0d6] sm:hidden  z-50 fixed w-full shadow-[0_4px_10px_rgba(0,0,0,0.1)]">
         <div className="flex items-center p-2 sm:hidden bg-[#f7f0d6]  w-[70vw]">
           <div className="flex items-center w-24 h-12 border-1 bg-[#f0f0e9] border-[#FFBE0B]  border-r-0 rounded-[10px] rounded-r-none">
             <img src={banner} className="w-24 h-14 " />
@@ -44,23 +47,27 @@ export default function () {
           >
             <path d="M2.25 2.25a.75.75 0 0 0 0 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 0 0-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 0 0 0-1.5H5.378A2.25 2.25 0 0 1 7.5 15h11.218a.75.75 0 0 0 .674-.421 60.358 60.358 0 0 0 2.96-7.228.75.75 0 0 0-.525-.965A60.864 60.864 0 0 0 5.68 4.509l-.232-.867A1.875 1.875 0 0 0 3.636 2.25H2.25ZM3.75 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0ZM16.5 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0Z" />
           </svg>
-
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            className="size-8"
-          >
-            <path
-              fillRule="evenodd"
-              d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z"
-              clipRule="evenodd"
-            />
-          </svg>
+          <button>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="size-8"
+            >
+              <path
+                fillRule="evenodd"
+                d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </button>
         </div>
       </header>
 
-      <div className="hidden sm:flex text-[20px] justify-between items-center bg-[#fff9ec] shadow-[0_4px_10px_rgba(0,0,0,0.1)] p-2">
+      {/* HEADER FOR LARGE SCREEN
+       */}
+
+      <div className="hidden sm:fixed sm:z-50 w-full sm:flex text-[20px] justify-between items-center bg-[#fff9ec] shadow-[0_4px_10px_rgba(0,0,0,0.1)] ">
         <img src={banner} alt="" className="w-24 md:w-32" />
 
         <ul className="lg:flex hidden justify-between md:gap-8 gap-2 md:text-[1.4rem] font-medium text-[1rem]">
@@ -82,7 +89,10 @@ export default function () {
         <div className="flex justify-between gap-3.5 text-[1.5rem] m-1">
           <FaHeart className="wishlist-icon" />
           <FaShoppingCart className="cart-icon" />
-          <FaUser className="profile-icon" />
+
+          <button>
+            <FaUser className="profile-icon" />
+          </button>
         </div>
       </div>
     </>
